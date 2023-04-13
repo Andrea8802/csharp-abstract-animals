475d9201243109e59963644eff7f93f69ec05bf3
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-13 15:49:38 +0200

    classe astratta

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("ZZZZZZ");
+        }
+
+        public abstract void Verso();
+        public abstract void Mangia();
+    }
+
+    public class Cane : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Woof Woof!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Carne");
+        }
+    }
+
+    public class Passerotto : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Cip Cip!");
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Semi");
+        }
+
+    }
+
+    public class Aquila : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Skree!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Lepre");
+        }
+    }
+
+    public class Delfino : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("weee-eeee-eeee-oooo-oooo-oooo-eeee-eeee-eeee");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Pesce");
+        }
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Animale cane, passerotto, aquila, delfino;
+
+
+            Console.WriteLine("Il cane fa: ");
+            cane = new Cane();
+            cane.Dormi();
+            cane.Verso();
+            cane.Mangia();
+
+            Console.WriteLine("\n \nIl passerotto fa: ");
+            passerotto = new Passerotto();
+            passerotto.Dormi();
+            passerotto.Verso();
+            passerotto.Mangia();
+
+            Console.WriteLine("\n \nIl aquila fa: ");
+            aquila = new Aquila();
+            aquila.Dormi();
+            aquila.Verso();
+            aquila.Mangia();
+
+
+            Console.WriteLine("\n \nIl delfino fa: ");
+            delfino = new Delfino();
+            delfino.Dormi();
+            delfino.Verso();
+            delfino.Mangia();
+
         }
     }
 }
